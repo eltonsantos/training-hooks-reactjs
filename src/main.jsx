1dc94abb3264b236b93a6bc60f56d6d6bcbac730
Elton Santos <eltonaxl@hotmail.com>
2023-02-04 00:18:09 -0300

    Create Exemplo07_2_useContext - Exemplo com context

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Exemplo07_1_useContext } from "./hooks/Exemplo07_1_useContext";
+import { Exemplo07_2_useContext } from "./hooks/Exemplo07_2_useContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Exemplo07_1_useContext />
+    <Exemplo07_2_useContext />
   </React.StrictMode>
 );
