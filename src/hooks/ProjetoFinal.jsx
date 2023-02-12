17fb299e706d9b8fd5c6ea9821468419ab3c2096
Elton Santos <eltonaxl@hotmail.com>
2023-02-12 13:56:01 -0300

    Styling component

--- src/hooks/ProjetoFinal.jsx
+++ src/hooks/ProjetoFinal.jsx
@@ -1,3 +1,14 @@
 export function ProjetoFinal() {
-  return <div>Teste</div>;
+  return (
+    <form>
+      <input type="text" placeholder="Nome" className="form-control" />
+      <input type="text" placeholder="Idade" className="form-control" />
+      <input type="text" placeholder="Cidade" className="form-control" />
+
+      <input type="button" value="Cadastrar" className="btn btn-primary" />
+      <input type="button" value="Alterar" className="btn btn-secondary" />
+      <input type="button" value="Excluir" className="btn btn-danger" />
+      <input type="button" value="Cancelar" className="btn btn-success" />
+    </form>
+  );
 }
