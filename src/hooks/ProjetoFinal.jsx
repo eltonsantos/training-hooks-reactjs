241008e50b7f915953c3551e62fa97d36f7802dd
Elton Santos <eltonaxl@hotmail.com>
2023-02-12 14:54:25 -0300

    Search by name

--- src/hooks/ProjetoFinal.jsx
+++ src/hooks/ProjetoFinal.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export function ProjetoFinal({
   btnCadastrar,
   setNome,
@@ -74,35 +76,46 @@ export function ProjetoFinal({
 }
 
 function Tabela({ vetor, selecionar }) {
+  const [termo, setTermo] = useState("");
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Nome</th>
-          <th>Idade</th>
-          <th>Cidade</th>
-          <th>Selecionar</th>
-        </tr>
-      </thead>
-      <tbody>
-        {vetor.map((obj, indice) => (
-          <tr key={indice}>
-            <td>{indice + 1}</td>
-            <td>{obj.nome}</td>
-            <td>{obj.idade}</td>
-            <td>{obj.cidade}</td>
-            <td>
-              <button
-                className="btn btn-success"
-                onClick={() => selecionar(indice)}
-              >
-                Selecionar
-              </button>
-            </td>
+    <div>
+      <input
+        type="text"
+        placeholder="Informe o nome"
+        className="form-control pesquisa"
+        onChange={(e) => setTermo(e.target.value)}
+      />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Cidade</th>
+            <th>Selecionar</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {vetor
+            .filter((obj) => obj.nome.includes(termo))
+            .map((obj, indice) => (
+              <tr key={indice}>
+                <td>{indice + 1}</td>
+                <td>{obj.nome}</td>
+                <td>{obj.idade}</td>
+                <td>{obj.cidade}</td>
+                <td>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => selecionar(indice)}
+                  >
+                    Selecionar
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
